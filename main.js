ebae8adbb7610a8596e99074b0ea1c9100b54a87
davide <dacapoccia@gmail.com>
2022-07-22 00:45:33 +0200

    !img

---
+++ main.js
@@ -0,0 +1,12 @@
+var app = new Vue (
+    {
+        el: '#app',
+        data: {
+            message: 'Hieronymus Bosch',
+            container: 'mainContent',
+            img01: 's-bosch.jpeg',
+            center: 'textCenter',
+            bosch: 'vwVh'
+        }
+    }
+);
